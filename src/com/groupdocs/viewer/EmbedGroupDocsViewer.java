a2be4d3e9007e051d6f355479909b41e20244483
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-06-24 09:10:58 +0300

    Added plug-in skeleton files

---
+++ src/com/groupdocs/viewer/EmbedGroupDocsViewer.java
@@ -0,0 +1,18 @@
+package com.groupdocs.viewer;
+
+import com.dotmarketing.plugin.PluginDeployer;
+import com.dotmarketing.portlets.structure.factories.FieldFactory;
+import com.dotmarketing.portlets.structure.factories.StructureFactory;
+import com.dotmarketing.portlets.structure.model.Field;
+import com.dotmarketing.portlets.structure.model.Structure;
+
+public class EmbedGroupDocsViewer implements PluginDeployer {
+    public boolean deploy() {
+        // TODO: Need to implement
+        return false;
+    }
+
+    public boolean redeploy(String version) {
+        return false;
+    }
+}
