c74f2a52bd4c1a0df345d26f8d51640f1f59cf89
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-08-05 01:22:25 +0300

    Implemented plugin functional

--- src/com/groupdocs/viewer/EmbedGroupDocsViewer.java
+++ src/com/groupdocs/viewer/EmbedGroupDocsViewer.java
@@ -1,10 +1,5 @@
 package com.groupdocs.viewer;
 
-import com.dotmarketing.plugin.PluginDeployer;
-import com.dotmarketing.portlets.structure.factories.FieldFactory;
-import com.dotmarketing.portlets.structure.factories.StructureFactory;
-import com.dotmarketing.portlets.structure.model.Field;
-import com.dotmarketing.portlets.structure.model.Structure;
 import org.apache.velocity.tools.view.tools.ViewTool;
 
 public class EmbedGroupDocsViewer implements ViewTool {
@@ -13,7 +8,11 @@ public class EmbedGroupDocsViewer implements ViewTool {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public String testMethod() {
-        return "Test Out";
+    public String RenderIframe(String fileGuid, String quality, String usePdf, String download, String frameborder, int width, int height) {
+        return "<iframe src=\"https://apps.groupdocs.com/document-viewer/Embed/" + fileGuid + "?quality=" + quality + "&use_pdf=" + usePdf + "&download=" + download + "&referer=DotCms-Viewer/1.0.0\" frameborder=\"" + frameborder + "\" width=\"" + Integer.toString(width) + "\" height=\"" + Integer.toString(height) +  "\">If you can see this text, your browser does not support iframes. Please enable iframe support in your browser or use the latest version of any popular web browser such as Mozilla Firefox or Google Chrome. For more help, please check our documentation Wiki: <a href=\"http://groupdocs.com/docs/display/Viewer/GroupDocs+Viewer+Integration+with+3rd+Party+Platforms\">http://groupdocs.com/docs/display/Viewer/GroupDocs+Viewer+Integration+with+3rd+Party+Platforms</a></iframe>";
+    }
+
+    public String RenderIframe(String fileGuid, int width, int height) {
+        return RenderIframe(fileGuid, "50", "False", "False", "0", width, height);
     }
 }
